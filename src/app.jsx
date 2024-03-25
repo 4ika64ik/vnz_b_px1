7b45375158f52996e8e327c925971a6f3edbe17d
John Doe <k3655692@gmail.com>
2024-03-25 15:20:14 +0200

    sfsdf

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,40 @@
-import { Route, Routes, useParams, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { FacebookPixel } from 'react-facebook-pixel';
 import TheForm from '@/pages/form/TheForm.jsx';
 import Home from '@/pages/home/Home.jsx';
-import ReactPixel from 'react-facebook-pixel';
 
 export default function App() {
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const pixelId = params.get('px');
+
+    // Используем событие deviceready Cordova для инициализации FacebookPixel
+    document.addEventListener('deviceready', () => {
+        // Проверяем, есть ли идентификатор пикселя
+        if (pixelId) {
+            try {
+                // Пытаемся инициализировать FacebookPixel
+                FacebookPixel.init(pixelId);
+                FacebookPixel.pageView();
+            } catch (error) {
+                // Обрабатываем ошибку инициализации
+                console.error('Ошибка инициализации FacebookPixel:', error);
+            }
+        } else if (location.pathname === '/') {
+            try {
+                // Пытаемся отследить просмотр страницы, если идентификатор пикселя отсутствует на главной странице
+                FacebookPixel.pageView();
+            } catch (error) {
+                // Обрабатываем ошибку отслеживания
+                console.error('Ошибка отслеживания просмотра страницы с помощью FacebookPixel:', error);
+            }
+        }
+    });
+
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/form" element={<FormWithPixel />} />
+            <Route path="/form" element={<TheForm pixelId={pixelId} />} />
         </Routes>
     );
-}
-
-function FormWithPixel() {
-    const location = useLocation();
-    let { px } = useParams(); // Извлечение значения px из строки запроса URL
-
-    React.useEffect(() => {
-        if (px) {
-            ReactPixel.init(px);
-            ReactPixel.track('ViewContent');
-        }
-    }, [px]);
-
-    return <TheForm pixelId={px} />;
 }
