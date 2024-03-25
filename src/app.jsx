78a28ec768dfcb618d1d36685e006e03040a4d4b
John Doe <k3655692@gmail.com>
2024-03-25 14:55:02 +0200

    ssdsdfsdf

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useParams, useLocation } from 'react-router-dom';
 import TheForm from '@/pages/form/TheForm.jsx';
 import Home from '@/pages/home/Home.jsx';
+import ReactPixel from 'react-facebook-pixel';
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/form" element={<TheForm />} />
+            <Route path="/form" element={<FormWithPixel />} />
         </Routes>
     );
+}
+
+function FormWithPixel() {
+    const location = useLocation();
+    let { px } = useParams(); // Извлечение значения px из строки запроса URL
+
+    React.useEffect(() => {
+        if (px) {
+            ReactPixel.init(px);
+            ReactPixel.track('ViewContent');
+        }
+    }, [px]);
+
+    return <TheForm pixelId={px} />;
 }
